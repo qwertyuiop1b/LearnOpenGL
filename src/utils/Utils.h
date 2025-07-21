bbe0ac0bc02d8255f9878a9d37e61feb6ab914f4
huanghuiqiang <1669903552@qq.com>
2025-07-21 19:14:12 +0800

    add 03_1_TextureSprite todo complete

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -5,9 +5,14 @@
 #ifndef OPENGL_UTILS_H
 #define OPENGL_UTILS_H
 
+#include <GLFW/glfw3.h>
+
 
 class Utils {
-
+public:
+    static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
 };
 
 
